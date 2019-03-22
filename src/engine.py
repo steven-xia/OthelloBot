aaac342bf6ce058b240699dc93846bd3a896730c
Steven <stevenxia@protonmail.com>
2019-03-21 23:47:03 -0700

    Implemented move ordering with the previous principal variation.
    Used the principal variation of the previous search (because iterative deepening :) as the first search to set a high bar for alpha-beta.

--- src/engine.py
+++ src/engine.py
@@ -6,6 +6,7 @@ A = ord("A")
 COORD_TO_BITBOARD = {chr(A + l) + str(n + 1): 1 << (board.BOARD_SIZE * n + l)
                      for l in range(board.BOARD_SIZE) for n in range(board.BOARD_SIZE)}
 COORD_TO_BITBOARD["None"] = None
+COORD_TO_BITBOARD["NONE"] = None
 
 BITBOARD_TO_COORD = {v: k for k, v in COORD_TO_BITBOARD.items()}
 
@@ -22,10 +23,13 @@ class Engine:
         self.board = board_object
         self.evaluator = evaluator
 
+        self.previous_search_position = board_object.get_board()
+        self.previous_principal_variation = ()
+
         self.TRANSPOSITION_TABLE = {}
         self.searched_nodes = 0
 
-    def negamax(self, board_object, depth, alpha, beta, color):
+    def negamax(self, board_object, depth, alpha, beta, color, pv=()):
         board_key = board_object.get_board()
 
         if depth == 0:
@@ -33,32 +37,37 @@ class Engine:
                 evaluation, _ = self.TRANSPOSITION_TABLE[board_key]
             except KeyError:
                 evaluation = self.evaluator(board_object)
-                self.TRANSPOSITION_TABLE[board_key] = evaluation, []
-            return color * evaluation, []
+                self.TRANSPOSITION_TABLE[board_key] = evaluation, ()
+            return color * evaluation, ()
 
         try:
             return self.TRANSPOSITION_TABLE[board_key]
         except KeyError:
             legal_moves = sorted(board_object.legal_moves(), key=lambda m: m in CORNERS, reverse=True)
 
+            if pv != ():
+                legal_moves = sorted(legal_moves, key=lambda m: m != pv[0])
+            else:
+                pv = (0, )
+
             value = -INFINITY
             for move in legal_moves:
                 self.searched_nodes += 1
 
                 board_object.move(move)
-                move_value, move_pv = self.negamax(board_object, depth - 1, -beta, -alpha, -color)
-                value = max(value, -move_value)
+                move_value, move_pv = self.negamax(board_object, depth - 1, -beta, -alpha, -color, pv[1:])
                 board_object.pop()
 
-                if -move_value == value:
+                if -move_value > value:
+                    value = -move_value
                     best_move = move
                     best_pv = move_pv
 
-                alpha = max(alpha, value)
-                if alpha >= beta:
-                    break
+                    alpha = max(alpha, value)
+                    if alpha >= beta:
+                        break
 
-            pv = [best_move] + best_pv
+            pv = (best_move, ) + best_pv
             self.TRANSPOSITION_TABLE[board_key] = value, pv
             return value, pv
 
@@ -66,8 +75,15 @@ class Engine:
         self.TRANSPOSITION_TABLE = {}
         self.searched_nodes = 0
 
+        if self.board.get_board() != self.previous_search_position:
+            self.previous_principal_variation = ()
+
         turn_factor = 1 if self.board.side == board.BLACK else -1
-        evaluation, pv = self.negamax(self.board, depth, -INFINITY, INFINITY, turn_factor)
+        evaluation, pv = self.negamax(self.board, depth, -INFINITY, INFINITY, turn_factor,
+                                      self.previous_principal_variation)
+
+        self.previous_search_position = self.board.get_board()
+        self.previous_principal_variation = pv
 
         return pv, evaluation
 
