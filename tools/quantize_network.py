ca851ae18300b86a550a0f95328fdf764c2e295b
Steven <stevenxia@protonmail.com>
2019-04-11 10:33:04 -0700

    Updated quantize network script.

--- tools/quantize_network.py
+++ tools/quantize_network.py
@@ -2,9 +2,12 @@ import os
 
 import tensorflow
 
-NETWORK_LOCATION = os.path.join("..", "src", "network.h5")
+NETWORK_DIRECTORY = os.path.join("..", "src")
 
-keras_model = tensorflow.keras.models.load_model(NETWORK_LOCATION)
+INPUT_FILENAME = os.path.join(NETWORK_DIRECTORY, "network.h5")
+OUTPUT_FILENAME = ".".join(INPUT_FILENAME.split(".")[:-1]) + ".tflite"
+
+keras_model = tensorflow.keras.models.load_model(INPUT_FILENAME)
 run_model = tensorflow.function(lambda x: keras_model(x))
 concrete_function = run_model.get_concrete_function(
     [tensorflow.TensorSpec([None, 8, 8, 2], tensorflow.float32, name="board_input"),
@@ -15,7 +18,7 @@ concrete_function = run_model.get_concrete_function(
 # concrete_function = model.signatures[
 #     tensorflow.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
 
-converter = tensorflow.lite.TFLiteConverter.from_keras_model_file(NETWORK_LOCATION)
+converter = tensorflow.lite.TFLiteConverter.from_keras_model_file(OUTPUT_FILENAME)
 converter.optimizations = [tensorflow.lite.Optimize.OPTIMIZE_FOR_LATENCY]
 # converter.optimizations = [tensorflow.lite.Optimize.OPTIMIZE_FOR_SIZE]
 tensorflow_lite_model = converter.convert()
